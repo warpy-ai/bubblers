a217a31d60392b560574f5c1a056045c72cf3d71
jucasoliveira <jucas.oliveira@gmail.com>
2024-05-14 14:18:56 +0100

    add change

--- examples/hello_world_example.rs
+++ examples/hello_world_example.rs
@@ -1,22 +1,25 @@
+use bubblers::{cli_builder, config::ArgConfig, config::CliConfig, config::CommandConfig};
 use std::sync::Arc;
 
-use bubblers::cli_builder;
-use bubblers::config::{ArgConfig, CliConfig, CommandConfig};
-
 fn main() {
-    let echo_action = Arc::new(|args: &[String]| {
+    // Define actions as functions for better readability and reuse
+    fn echo_action(args: &[String]) {
         println!("Echo: {}", args.join(" "));
-    }) as Arc<dyn Fn(&[String]) + Send + Sync + 'static>;
+    }
 
-    let version_action = Arc::new(|_: &[String]| {
+    fn version_action(_: &[String]) {
         println!("Version 1.0.0");
-    }) as Arc<dyn Fn(&[String]) + Send + Sync + 'static>;
+    }
 
+    // Create a new CLI configuration
     let mut cli = CliConfig::new("MyCLI", "1.0", "A simple CLI");
 
-    // Set up the echo command
-    let mut echo_command =
-        CommandConfig::new("echo", "Echo the input back to the console", echo_action);
+    // Define the echo command with its action and argument
+    let mut echo_command = CommandConfig::new(
+        "echo",
+        "Echo the input back to the console",
+        Arc::new(echo_action),
+    );
     echo_command.add_arg(ArgConfig {
         name: "message",
         help: "Message to echo back".to_string(),
@@ -28,24 +31,22 @@ fn main() {
     cli.add_command(CommandConfig::new(
         "version",
         "Display the version information",
-        version_action,
+        Arc::new(version_action),
     ));
 
+    // Build and parse the CLI
     let matches = cli_builder::build_cli(&cli).get_matches();
 
+    // Execute the matched command
     if let Some((command_name, sub_matches)) = matches.subcommand() {
-        let cmd = cli
-            .commands()
-            .iter()
-            .find(|cmd| cmd.name() == command_name)
-            .unwrap();
+        if let Some(cmd) = cli.commands().iter().find(|cmd| cmd.name() == command_name) {
+            let args = sub_matches
+                .get_many::<String>("message")
+                .unwrap_or_default()
+                .cloned()
+                .collect::<Vec<_>>();
 
-        let args = sub_matches
-            .get_many::<String>("message")
-            .unwrap_or_default()
-            .cloned()
-            .collect::<Vec<_>>();
-
-        cmd.execute_action(&args);
+            cmd.execute_action(&args);
+        }
     }
 }
